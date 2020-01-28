bcfb46faaae17f67acaeb9e604853e50b6b520a9
Angel Vivanco <avivanco01@aurora.edu>
2020-01-27 19:59:45 -0600

    Final Working Version for Codelab 2.2

--- app/src/main/java/com/example/twoactivities/secondActivity.java
+++ app/src/main/java/com/example/twoactivities/secondActivity.java
@@ -4,14 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class secondActivity extends AppCompatActivity {
+    private static final String LOG_TAG = secondActivity.class.getSimpleName();
+
     public static final String EXTRA_REPLY =
             "com.example.android.twoactivities.extra.REPLY";
     private EditText mReply;
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +65,7 @@ public class secondActivity extends AppCompatActivity {
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK,replyIntent);
+        Log.d(LOG_TAG, "ENd SecondActivity");
         finish();
     }
 }
